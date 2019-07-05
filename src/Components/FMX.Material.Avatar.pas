845cab486ce6d259d713056f5deebe4b09ada8ed
Rodrigo Bernardi <coloradonanet@unochapeco.edu.br>
2019-07-05 10:24:17 -0300

    adjusted font size

--- src/Components/FMX.Material.Avatar.pas
+++ src/Components/FMX.Material.Avatar.pas
@@ -139,9 +139,9 @@ begin
   try
     Canvas.FillEllipse(LRect, AbsoluteOpacity, FFill);
     if Self.Width > Self.Height then
-      Canvas.Font.Size := Self.Height / 1.5
+      Canvas.Font.Size := Self.Height / 2
     else
-      Canvas.Font.Size := Self.Width / 1.5;
+      Canvas.Font.Size := Self.Width / 2;
     Canvas.Font.Style := [TFontStyle.fsBold];
     Canvas.Fill.Color := TAlphaColorRec.White;
 
